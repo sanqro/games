e8fb974420d398c175134d696d707df162bc6e07
sanqro <contact@sanqro.xyz>
2024-01-25 10:25:09 +0100

    add auth0 > > Co-authored-by: Nicola <nicolaschule420@gmail.com>

--- games/src/app.d.ts
+++ games/src/app.d.ts
@@ -6,9 +6,16 @@ declare global {
 			export const PUBLIC_SUPABASE_ANON_KEY: string;
 			export const PUBLIC_SUPABASE_URL: string;
 		}
+		interface event {
+			locals: Locals
+		}
 		interface Locals {
 			sb: TypedSupabaseClient
 			session: Session | null
+			getSession: () => Promise<Session | null>
+		}
+		interface PageData {
+			session: import("@supabase/supabase-js").Session | null
 		}
 	}
 }
--- games/src/lib/supabase.ts
+++ games/src/lib/supabase.ts
@@ -1,12 +1,26 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
-import { createClient } from '@supabase/supabase-js'
-
-import {
-	PUBLIC_SUPABASE_ANON_KEY,
-	PUBLIC_SUPABASE_URL,
-} from "$env/static/public"
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
+import { createClient } from '@supabase/supabase-js';
+import type { Handle } from '@sveltejs/kit';
 
 export const supabaseClient = createClient(
 	PUBLIC_SUPABASE_URL,
 	PUBLIC_SUPABASE_ANON_KEY,
 )
+
+export const handle: Handle = async ({ event, resolve }) => {
+  event.locals.sb = supabaseClient;
+
+  event.locals.getSession = async () => {
+    const {
+      data: { session }
+    } = await event.locals.sb.auth.getSession();
+    return session;
+  };
+
+  return resolve(event, {
+    filterSerializedResponseHeaders(name: string) {
+      return name === 'content-range';
+    }
+  });
+};
+
